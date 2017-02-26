3f4ae7aa241fef56198a104f4b8cf06cff738eb1
ramjanarthan <ram.janarthan11@gmail.com>
2017-02-26 23:05:27 +0800

    Add storage class to facilitate DIP

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,20 @@
+package seedu.addressbook.storage;
+
+import java.nio.file.Path;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+/**
+ * Abstract class to facilitate DIP between 'Logic' and 'StorageFile'
+ * 
+ */
+
+public abstract class Storage {
+	
+	public abstract boolean isValidPath(Path filePath);
+	public abstract void save(AddressBook addressBook) throws StorageOperationException;
+	public abstract AddressBook load() throws StorageOperationException; 
+	public abstract String getPath();	
+
+}
